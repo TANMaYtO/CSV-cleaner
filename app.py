5157a74e046888869f59b58cf7ee43049fc40f7e
Tanmay <tomartanmay8@gmail.com>
2025-07-21 12:11:57 +0530

    adding encoding

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import io
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
 
 st.set_page_config(page_title="CSV Analyser", layout="centered")
 
@@ -15,6 +16,14 @@ if "cleaned_df" not in st.session_state:
     st.session_state.cleaned_df = None
 if 'filtered_data' not in st.session_state:
     st.session_state.filtered_data = None
+if 'dropped_data' not in st.session_state:
+    st.session_state.droppped_data = None
+if 'cat_col' not in st.session_state:
+    st.session_state.cat_col = None
+if 'num_col' not in st.session_state:
+    st.session_state.num_col = None
+if 'one_hot' not in st.session_state:
+    st.session_state.one_hot = None
 
 # Upload Section
 uploaded_file = st.file_uploader("📤 Upload your CSV file", type=["csv"])
@@ -67,16 +76,44 @@ if st.session_state.cleaned_df is not None:
 
 if st.session_state.filtered_data is not None:
     if st.button('Drop duplicates'):
-        df = st.session_state.filtered_data
-        df = df.drop_duplicates()
-        st.write("📐 New Shape:", df.shape)
+        dropped_df = st.session_state.filtered_data
+        dropped_df = df.drop_duplicates()
+        st.write("📐 New Shape:", dropped_df.shape)
+        st.session_state.dropped_data = dropped_df
 
-                # Download Button
+
+
+        # Download Button
         csv_buffer = io.StringIO()
-        filtered_df.to_csv(csv_buffer, index=False)
+        dropped_df.to_csv(csv_buffer, index=False)
         st.download_button(
-            label="📥 Download Cleaned CSV",
+            label="📥 Download Cleaned CSV(till dropped duplicates)",
             data=csv_buffer.getvalue(),
             file_name="cleaned.csv",
             mime="text/csv"
         )
+
+if st.session_state.dropped_data is not None:
+    if st.button('Divide in numerical and catagorical colomns'):
+        cat_col = dropped_df.select_dtypes("object")
+        num_col = dropped_df.select_dtypes(exclude="O")
+        st.session_state.cat_col = cat_col
+        st.session_state.num_col = num_col
+        st.write('catagorical colomn:')
+        st.dataframe(cat_col)
+        st.wrtie('numerical colomn:')
+        st.dataframe(num_col)
+        if st.session_state.cat_col is not None:
+            if st.button('Encode catagorical colomns'):
+                OHE = OneHotEncoder(drop= "if_binary")
+                cat_col_encoded = OHE.fit_transform(cat_col).toarray()
+                column_name = list(OHE.get_feature_names_out())
+                one_hot = pd.DataFrame(cat_col_encoded,columns=column_name)
+                st.write('encoded catagotical colomns:')
+                st.dataframe(one_hot)
+                st.session_state.one_hot = one_hot
+
+                if st.session_state.one_hot is not None:
+                    if st.button()
+
+
